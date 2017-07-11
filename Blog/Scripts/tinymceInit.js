aee9a5201e2eca97e60c733372e7efac285e1147
Usurer <ivvlg@narod.ru>
2017-07-11 21:09:06 +0300

    Fancy animation added to prevent the flash of unstyled content on Editr UI.

--- Blog/Scripts/tinymceInit.js
+++ Blog/Scripts/tinymceInit.js
@@ -1,17 +1,24 @@
-﻿tinymce.init({
-    selector: 'textarea',
-    height: 500,
-    menubar: false,
-    plugins: [],
-    toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    content_css: [],
-    setup: function (editor) {
-        editor.on('init', function (e) {
-            console.log('Editor was initialized.');
+﻿(function () {
+    $(document).ready(function () {
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            menubar: false,
+            plugins: [],
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            content_css: [],
+            setup: function (editor) {
+                editor.on('init', function (e) {
+                    editor.on('Change', function (e) {
+                        BlogApp.contentEditor.pushPreview();
+                    });
 
-            editor.on('Change', function (e) {
-                BlogApp.contentEditor.pushPreview();
-            });
-        });
-    }
-});
+                    //$('.js-editor-form').removeClass('hidden');
+                    $('.js-editor-form').addClass('animation-show');
+                    $('.js-editor-form').removeClass('hidden');
+                });
+            }
+        });        
+    });
+})()
+
